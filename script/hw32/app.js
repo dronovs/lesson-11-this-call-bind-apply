cbabd0de5378473edf042f35f047dd5a201daf78
dronovs <od270891dss@gmail.com>
2022-04-27 19:24:25 +0300

    update

---
+++ script/hw32/app.js
@@ -0,0 +1,27 @@
+'use strict'
+
+let calculator = {
+    askOperand () {
+        let number = +prompt('Please, enter an operand');
+        if (isNaN(number)) {
+            while(isNaN(number)) {
+                number = +prompt('Please, enter an operand');
+            }
+        }
+        return number;
+    },
+    sum () {
+        return this.numberOne + this.numberTwo;
+    },
+    mul () {
+        return this.numberOne * this.numberTwo;
+    },
+    read () {
+        this.numberOne = this.askOperand();
+        this.numberTwo = this.askOperand();
+    },
+}
+calculator.read();
+// console.log(calculator.sum());
+console.log(calculator.mul());
+console.log(calculator);
